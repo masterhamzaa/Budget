62b6d6948b1ee95330c9c0cb35efc3dbebaea319
masterhamzaa <master.hamza4576@gmail.com>
2022-10-29 22:00:58 +0100

    up to gitt

--- src/compo/Footer.jsx
+++ src/compo/Footer.jsx
@@ -4,7 +4,7 @@ import Styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <>
-      <section className={Styles.quote}>m i n i p r o j e t</section>
+      <section className={Styles.quote}> m i n i p r o j e t </section>
       <footer className={Styles.footer}>Hamza Mimih &copy; 2022</footer>
     </>
   );
